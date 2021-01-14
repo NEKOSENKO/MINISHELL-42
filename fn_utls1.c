64b1b65572d83d8d579a111037fc61c5239052d7
nekosenko <brija.main@gmail.com>
2021-01-15 00:55:00 +0100

    saving these so i don't forget them

--- fn_utls1.c
+++ fn_utls1.c
@@ -71,3 +71,40 @@ char	*ft_strjoin(char *s1, char *s2)
 	p[i] = 0;
 	return (p);
 }
+
+void *def_not_calloc(size_t size)
+{
+	void *ptr;
+
+	if (!(ptr = malloc(size)));
+		return NULL;
+	ft_bzero(ptr, size);
+	return ptr;
+}
+
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = -1;
+	while (++i < n)
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+	return (dst);
+}
+
+void *senko_realloc(void *ptr, size_t old_s, size_t new_s)
+{
+	void *new;
+
+	if (!ptr)
+		return NULL;
+	if (!(new = def_not_calloc(new_s)))
+	{
+		ft_memcpy(new, ptr, old_s < new_s ? old_s : new_s);
+		free(ptr);
+		return NULL;
+	}
+	
+}
+
